260ee9ea866c31d8024c35f8319507161fb614f3
Giuliopime <giuliopime@gmail.com>
2022-08-02 00:34:37 +0200

    feat: dark mode support & alert fixing

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="h-full flex flex-col items-center justify-center">
     <div
         :class="[alertText === null ? 'opacity-0 translate-y-full' : 'opacity-100 translate-y-0', alertType === 1 ? 'bg-green-100 text-green-700' : (alertType === 2 ? 'bg-red-100 text-red-700' : 'bg-yellow-100 text-yellow-700')]"
-        class="fixed z-10 bottom-2 md:bottom-[7%] rounded-lg py-5 px-6 mb-4 text-base mb-3 transition-opacity transition-all ease-in-out duration-500"
+        class="fixed z-10 bottom-4 md:bottom-[7%] rounded-lg py-5 px-6 m4-2 mx-4 text-base text-center transition-opacity transition-all ease-in-out duration-500"
         @click="alertText=null"
     >
       {{ alertText }}
@@ -14,7 +14,7 @@
     <div class="text-center">
       <span class="italic text-xl md:text-2xl">The essential app for any kind of list</span>
     </div>
-    <form @submit.prevent class="w-10/12  md:w-auto text-white flex items-center content-center flex-col md:flex-row bg-stone-700 hover:bg-stone-700/95 text-lg md:text-xl mt-20 rounded-md p-2">
+    <form @submit.prevent class="w-10/12  md:w-auto text-white flex items-center content-center flex-col md:flex-row bg-stone-700 dark:bg-stone-800 dark:hover:bg-stone-800/70 hover:bg-stone-700/95 text-lg md:text-xl mt-20 rounded-md p-2 duration-300">
       <input v-model="email" class="outline-0 p-3 bg-transparent w-full md:w-auto" type="email" placeholder="> enter your email" required>
       <button type="submit" class="button w-full md:w-auto font-medium" @click="notifyEmail">Notify when ready!</button>
     </form>
