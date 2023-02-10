ab978090636c92d42606d0ac9acb1c806251a60f
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-02-10 02:40:06 +0100

    feat: email verified basic page

---
+++ pages/email-verified.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-full flex grow flex-col items-center justify-center text-center">
+    <span class="text-3xl leading-none md:text-6xl font-bold animate-pulse">Email verified!</span>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: 'Email successful verification page',
+  meta: [
+    { name: 'title', content: 'Email successful verification page' }
+  ],
+})
+</script>
