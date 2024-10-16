20ad0b3fd3c0b27e47ffc8da163d337e0d1246fb
Giuliopime <giuliopime@gmail.com>
2024-10-16 17:07:48 +0200

    fix: dot

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center">
     <div class="flex min-h-screen flex-col items-center justify-center">
       <span class="text-[4.8rem] leading-none md:text-8xl mb-10 font-bold text-center px-8 md:px-16">
-        .It's coming.
+        It's coming.
       </span>
       <span class="italic text-xl md:text-2xl px-8 text-center">The essential app for any kind of list, currently under development</span>
     </div>
